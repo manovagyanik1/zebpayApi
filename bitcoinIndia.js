656715c7ed8aa16251ca64458c64efe51a9e6062
shubham agrawal <manovagyanik1@gmail.com>
2017-11-03 22:23:35 +0530

    making it run on android termux

--- bitcoinIndia.js
+++ bitcoinIndia.js
@@ -2,7 +2,7 @@ var request = require("request");
 var rp = require('request-promise');
 var cheerio = require("cheerio");
 var cron = require('node-cron');
-var say = require('say');
+// var say = require('say');
 
 var FgGreen = "\x1b[32m";
 var FgYellow = "\x1b[33m";
@@ -30,16 +30,16 @@ cron.schedule('*/10 * * * * *', function(){
         console.log(FgGreen, "sell:", sellValue);
         console.log(Reset, "********************");
 
-        if(buyValueInInteger > 493000) {
-            say.speak("alert negative: buy value is more than 493,000");
-        } else if (buyValueInInteger < 487000) {
-            say.speak("alert positive: buy value is less than 486,000");
-        }
-        if(sellValueInInteger > 490000) {
-            say.speak("alert negative: sell value is greater than 490,000");
-        } else if(sellValueInInteger > 490000) {
-            say.speak("alert: sell value is greater than 490,000");
-        }
+        // if(buyValueInInteger > 493000) {
+        //     say.speak("alert negative: buy value is more than 493,000");
+        // } else if (buyValueInInteger < 487000) {
+        //     say.speak("alert positive: buy value is less than 486,000");
+        // }
+        // if(sellValueInInteger > 490000) {
+        //     say.speak("alert negative: sell value is greater than 490,000");
+        // } else if(sellValueInInteger > 490000) {
+        //     say.speak("alert: sell value is greater than 490,000");
+        // }
     })
     .catch(function (err) {
        console.log("error occured");
